c29b51e409d0092e124bd3785b13b61799f6bd69
Patricia Narvaez <patricia@checkpointone.ca>
2021-11-26 14:49:26 -0800

    Data needed for test

--- Program.cs
+++ Program.cs
@@ -11,6 +11,33 @@ namespace Dev_Test_Nov_2021
     {
         static void Main(string[] args)
         {
+            //Part 2
+            List<Product> products = new List<Product> {
+                new Product {Id = "B091NE9K3", Price =59.96, Quantity = 5},
+                new Product {Id = "B091NEGU3", Price =7.05, Quantity = 10},
+                new Product {Id = "B091NEEC3", Price =6.38, Quantity = 15},
+                new Product {Id = "B091NE9S3", Price =13.25, Quantity = 23},
+                new Product {Id = "B091NE9K4", Price =99.96, Quantity = 2},
+                new Product {Id = "B091NEGU4", Price =22.83, Quantity = 150},
+                new Product {Id = "B091NEEC4", Price =19.18, Quantity = 45},
+                new Product {Id = "B091NE9S4", Price =28.88, Quantity = 345},
+                new Product {Id = "B091NE9K5", Price =49.99, Quantity = 589},
+                new Product {Id = "B091NEGU5", Price =12.05, Quantity = 25},
+            };
+
+            //Part 3 
+            int[] ordersIds = { 1, 2, 3, 4 };
+            double[] ordersValues = { 25.5, 92.5, 78.23, 12.95, 83.67 };
+            string[] ordersCustomers = { "Tracy Erkut", "Arvin Aitken", "Ryan Mae", "Sherri Smith", "Adam Weller" };
         }
+    }
+
+    class Product
+    {
+        public string Id { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
